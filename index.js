66852ceaa6e12102ec44cf88186698b61994043b
Yaroslav Zahorodnyi <zahorodnyi.yaroslav.work@gmail.com>
2024-01-10 13:30:16 +0100

    Tutorial #1. Setup, Web scraping & Testing

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+         headless: true,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto('https://www.google.com');
+    await page.screenshot({ path: 'google.png' });
+
+    await browser.close();
+})();
